5fa6925dfe62b2ed8d9051c101885db5daaa0e94
Ales Rechtorik <foxxcz@gmail.com>
2022-11-12 10:28:50 +0100

    add redis

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -1,18 +1,23 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import { createClient } from 'redis';
 import * as express from 'express';
 
 const app = express();
+const client = createClient();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to app!' });
+client.on('error', (err) => console.log('Redis Client Error', err));
+client.on('connect', () => console.log('Redis Client Connected!'));
+
+app.get('/api', async (_, res) => {
+  await client.set('time', 'value');
+  const value = await client.get('time');
+
+  res.send({ message: `Welcome to app! ${value}` });
 });
 
 const port = process.env.port || 3333;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
