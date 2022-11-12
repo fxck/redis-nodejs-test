9c5c07b0bee898dcb9558a5be629b014a251915e
Ales Rechtorik <foxxcz@gmail.com>
2022-11-12 10:45:23 +0100

    wip

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -2,16 +2,21 @@ import { createClient } from 'redis';
 import * as express from 'express';
 
 const app = express();
+
+console.log('process env', process.env.db_connectionString);
+
 const client = createClient({
-  url: process.env.db_connectionString
+  url: process.env.db_connectionString || 'redis://db:6379'
 });
 
 client.on('error', (err) => console.log('Redis Client Error', err));
 client.on('connect', () => console.log('Redis Client Connected!'));
 
 app.get('/api', async (_, res) => {
+  await client.connect();
   await client.set('time', 'value');
   const value = await client.get('time');
+  await client.disconnect();
 
   res.send({ message: `Welcome to app! ${value}` });
 });
